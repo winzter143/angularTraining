122ea74bb7d9d74153a9980bc5ea0ab2fe1c1977
syntax3rror <winzter@syntax3rror.co.cc>
2019-07-13 10:56:04 +0800

    Adding Lifecycle component OnInit
       modified:   src/app/app.component.html
       modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'HeroTraining';
+
+//  Implement 
+export class AppComponent  implements  OnInit{
+  title = 'Angular Training XXX';
+
+  constructor(){}
+
+
+  ngOnInit(){
+    // Before 
+  }
+
+
 }
+
+
+
