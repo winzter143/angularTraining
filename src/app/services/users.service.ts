20b24c8c41b42fb65e83a8270c0e6f8b35d0fd56
syntax3rror <winzter@syntax3rror.co.cc>
2019-07-20 10:35:09 +0800

    Adding Post/Put/delete    modified:   src/app/services/users.service.ts

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -21,10 +21,37 @@ export class UsersService {
 
     getHeroes(): Observable<User[]> {
         this.http.get(`${baseUrl}/users`).pipe(
-           map.data((data: User[])) => (
+           map.data((data: User[])) => ( // you can use tap/map
              // could be use "tap" if no value will process/reassign
             return data;
            )
         );
+    }
+
+    postHeroes(user: User){
+        return this.http.post("/users", user, {httpHeaders})
+        .pipe(
+           tap((data) -> ){
+           return data;
+           }
+        )
+    }
+
+    putHeroes(user: User){
+        return this.http.put(`${baseUrl}/users/${user.id}`, user, {httpHeaders})
+        .pipe(
+           tap((data) -> ){
+           return data;
+           }
+        )
+    }
+
+    deleteHeroes(id: string){
+        return this.http.delete(`${baseUrl}/users/${id}`, user, {httpHeaders})
+        .pipe(
+           tap((data) -> ){
+           return data;
+           }
+        )
     }
 }
